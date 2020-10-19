f18e5b0470683e4ef99270daf0d1107d3d19c384
IlovePi314 <59897958+IlovePi314@users.noreply.github.com>
2020-10-19 13:14:13 -0500

    Update init.js

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -33,7 +33,7 @@ var init = function (window) {
             circles.push(circle);
         }
         // TODO 3 / 7 : Call the drawCircle() function 
-        for (var loopsCompleted = 0; loopsCompleted < 125; loopsCompleted++) {
+        for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
         drawCircle();
 }
        
