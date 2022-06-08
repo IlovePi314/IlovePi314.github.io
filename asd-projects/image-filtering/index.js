d60482f22d5b46331bd70fb279def9a1cbdd4ce5
IlovePi314 <brodriguez3141@gmail.com>
2022-06-08 21:30:03 +0000

    finnaly'

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,10 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-  applyFilter();
-
+  applyFilterNoBackground(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
+  
 
   // do not change the below line of code
   render($("#display"), image);
@@ -32,28 +34,52 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-function applyFilter() {
+function applyFilter(filterFunction) {
   for (var row = 0; row < image.length; row++) {
-    for (var col = 0; col < image[row].length; col++) {
-      var rgbString = image[col][row]
-      var rgbNumbers = rgbToStringToArray(rgbString);
-      rgbNumbers[RED] = 255
+    for (var col = 0; col < image[col].length; col++) {
+      var rgbString = image[row][col]
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
       rgbString = rgbArrayToString(rgbNumbers)
-      image[col][row] = rgbString
+      image[row][col] = rgbString
     }
   }
 }
 
 // TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+  var backgroundcolor = image[0][0]
+  for (var row = 0; row < image.length; row++) {
+    for (var col = 0; col < image[col].length; col++) {
+      var rgbString = image[row][col]
+      var rgbNumbers = rgbStringToArray(rgbString);
+      if (backgroundcolor != rgbString){
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers)
+      image[row][col] = rgbString}
+    }
+  }
 
+}
 
 // TODO 5: Create the keepInBounds function
+function keepInBounds(maybe){
+    maybe = Math.max(maybe, 0)
+    maybe = Math.min(maybe, 255)
+    return maybe;
+  }
 
 
 // TODO 3: Create reddify function
-
+function reddify(yes) {
+yes[RED] = 200
+}
 
 // TODO 6: Create more filter functions
-
-
+function decreaseBlue(perhaps){
+perhaps[BLUE] = keepInBounds(perhaps[BLUE] - 50 );
+}
+function increaseGreenByBlue(no){
+no[GREEN] = keepInBounds(no[BLUE] + no[GREEN])
+}
 // CHALLENGE code goes below here
