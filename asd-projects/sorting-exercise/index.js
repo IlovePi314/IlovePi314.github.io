b34f94c3bfc0d46232dea6fec692d96c09f768df
IlovePi314 <brodriguez3141@gmail.com>
2022-06-13 21:28:08 +0000

    reason to save i guess

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -14,17 +14,42 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+async function bubbleSort(array){
+    for (var i = 0; i <= array.length - 1; i++) {
+        for (var j = array.length - 1; j >= i + 1; j--){
+            if (array[j].value < array[j - 1].value){
+                swap(array, j, j - 1);
+                updateCounter(bubbleCounter);
+                await sleep();
+            }
+        }
+    }
+}
 
 // TODO 3: Implement quickSort
+async function quickSort(array, left,right) {
+    if (right - left > 0) {
+        var index = await partition(array, left, right);
+        if (left < index - 1){
+           await quickSort(array, left, index - 1);  
+        }
+        if (right > index){
+            await quickSort(array, index, right)
+        }
+    }
 
+}
 
 // TODOs 4 & 5: Implement partition
 
 
 // TODO 1: Implement swap
-
-
+function swap (array, i, j) {
+var temp = array[i];		
+array[i] = array[j]
+array[j] = temp;	
+drawSwap(array, i, j);
+}
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
 ///////////////////////////////////////////////////////////////////////
