d273c765e3b9354340d40e7b8fd0c7d64d8dba58
IlovePi314 <brodriguez3141@gmail.com>
2022-06-14 19:51:58 +0000

    done sorting

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -41,7 +41,26 @@ async function quickSort(array, left,right) {
 }
 
 // TODOs 4 & 5: Implement partition
+async function partition(array, left, right) {
+   var pivot = array[Math.floor((right + left)/2)].value;
+    while (left < right) {
+        while (array[left].value < pivot) {
+            left++; 
+        }
+        while (array[right].value > pivot){
+            right--
+            
+        }
+        if (left < right){
+            swap(array, left, right);
+            updateCounter(quickCounter);
+            await sleep();
+        }
 
+    }
+
+    return left + 1;
+}
 
 // TODO 1: Implement swap
 function swap (array, i, j) {
